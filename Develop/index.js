cdd1d71d01af92c721db29bb400b69de5c146391
Cadenza <mirandalucsb@gmail.com>
2021-12-24 17:54:50 -0700

    got started with basic functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of your project?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data, err =>{
+        if (err) throw new Error(err);
+        console.log('Page created!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
